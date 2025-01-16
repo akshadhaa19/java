ee742ac6f1130ddb3cf281e27bdb51905e1a1cad
akshadhaa19 <akshadharavindran@gmail.com>
2025-01-16 12:52:33 +0530

    changes

--- challenges/Quiz.java
+++ challenges/Quiz.java
@@ -10,33 +10,53 @@ public class Quiz {
 
         System.out.println("1. Which country held the 2016 Summer Olympics?");
         System.out.println("\ta) China\n\tb) Ireland\n\tc) Brazil\n\td) Italy\n");
-        //store answer 1
+        String firstanswer = scan.nextLine();
 
         System.out.println("\n2. Which planet is the hottest?");
         System.out.println("\ta) Venus\n\tb) Saturn\n\tc) Mercury\n\td) Mars\n");
-        //store answer 2
+        String secondanswer = scan.nextLine();
 
         System.out.println("\n3. What is the rarest blood type?");
         System.out.println("\ta) O\n\tb) A\n\tc) B\n\td) AB-Negative\n");
-        //store answer 3
+        String thirdanswer = scan.nextLine();
 
         System.out.println("\n4. Which one of these characters is friends with Harry Potter?");
         System.out.println("\ta) Ron Weasley\n\tb) Hermione Granger\n\tc) Draco Malfoy\n");
-        //store answer 4
+        String fourthanswer = scan.nextLine();
 
         int score = 0;
 
-
-        //Task 2: Check each answer - For each correct answer, add 5 points to the score. 
-        
+        if(firstanswer.equals("c"))
+        {
+            score += 5;
+        }
+        else if (secondanswer.equals("a"))
+        {
+            score+=5;
+        }
+        else if(thirdanswer.equals("d"))
+        {
+            score+=5;
+        }
+        else if(fourthanswer.equals("a") || fourthanswer.equals("b"))
+        {
+            score+=5;
+        }
         System.out.println("Your final score is: " + score + "/20");
 
-        //Task 3: print a message depending on the score
-
-        // if the score is 15 or higher, print: "Wow, you know your stuff!";
-        // if the score is between 5 and 15, print "Not bad!";
-        // else, better luck next time. 
-
+        if(score >=15)
+        {
+            System.out.println("wow");
+        }
+        else if (score>5 && score<15)
+        {
+            System.out.println("not bad");
+        }
+        else
+        {
+            System.out.println("better luck");
+        }
+        
 
         scan.close();
 
