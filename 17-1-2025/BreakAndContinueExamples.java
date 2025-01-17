62977d73e1198fc942bfc12a68fd4fa810285bd7
akshadhaa19 <akshadharavindran@gmail.com>
2025-01-17 14:49:47 +0530

    new

--- 17-1-2025/BreakAndContinueExamples.java
+++ 17-1-2025/BreakAndContinueExamples.java
@@ -12,7 +12,27 @@ public class BreakAndContinueExamples {
         System.out.println(i);
        } 
 
+       System.out.println("\n even numbers");
+       for(int i=1;i<21;i++)
+       {
+        if(i%2==0)
+        {
+            continue;
+        }
+        System.out.println(i);
+       }
+
+       for(int i=21;i<30;i++)
+       {
+        if(i%3==0)
+        {
+            System.out.println(i);
+            break;
+        }
+       }
+
     }
     
     
 }
+
